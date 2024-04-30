709d99857dd09c9c08870b88981a12ea1e5a1aa9
Edward Vargas <edthevargas@gmail.com>
2024-04-30 16:23:26 -0400

    header is set with placeholder elements

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,2 +1,22 @@
 import "./header.scss";
-import Header from "./components/header/header";
+import homeIcon from "../../assets/homeIcon.svg";
+
+const header = () => {
+   return (
+      <div className="header">
+         <a href="/" className="header__logo">
+            <img src={homeIcon} className="homeIcon" alt="home icon" />
+         </a>
+         <div className="header__navbar">
+            <a href="/" className="navbarItem">
+               Compare
+            </a>
+            <a href="/" className="navbarItem">
+               Login
+            </a>
+         </div>
+      </div>
+   );
+};
+
+export default header;
