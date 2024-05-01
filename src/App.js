72ca88d8b6130c309c5b472abcca811c2ee19b0f
Edward Vargas <edthevargas@gmail.com>
2024-05-01 19:30:23 -0400

    added routing for input page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import Header from "./components/header/header";
+import InputPage from "./pages/inputPage/inputPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
    return (
       <div className="App">
          <Header />
+         <Router>
+            <Routes>
+               <Route path="/" element={<InputPage />} />
+            </Routes>
+         </Router>
       </div>
    );
 }
