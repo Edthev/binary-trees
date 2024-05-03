362cb5a02c3a83d542c3b2a8f3715d9e59cc9245
Edward Vargas <edthevargas@gmail.com>
2024-05-02 22:15:54 -0400

    texbox has placeholder text

--- src/pages/inputPage/inputPage.js
+++ src/pages/inputPage/inputPage.js
@@ -3,7 +3,11 @@ const inputPage = () => {
    return (
       <div className="inputPage">
          <form className="inputPage__form">
-            <input type="textbox" className="inputPage__textbox"></input>
+            <input
+               type="textbox"
+               className="inputPage__textbox"
+               placeholder="input array of tasks here..."
+            ></input>
             <button className="inputPage__button">Submit</button>
          </form>
       </div>
