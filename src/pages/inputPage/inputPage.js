ae596d35989f1cf6531371f14614eb0ec73c7125
Edward Vargas <edthevargas@gmail.com>
2024-05-02 22:07:38 -0400

    added textbox

--- src/pages/inputPage/inputPage.js
+++ src/pages/inputPage/inputPage.js
@@ -1,3 +1,9 @@
-const inputPage = () => {};
+const inputPage = () => {
+   return (
+      <div className="inputPage">
+         <input type="textbox" className="inputPage__textbox"></input>
+      </div>
+   );
+};
 
 export default inputPage;
